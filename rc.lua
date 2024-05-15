a60df2eeefdfc9047d63610b27eab263471f2ff2
Good1Cheese <2916675@gmail.com>
2024-05-15 22:22:08 +0300

    suspend to sus

--- rc.lua
+++ rc.lua
@@ -227,7 +227,7 @@ globalkeys = gears.table.join(
     awful.key({ modkey, "Shift" }, "`", function() awful.spawn("reboot") end,
         { description = "reboot", group = "awesome" }),
     awful.key({ modkey, "Shift" }, "s", function() awful.spawn("systemctl suspend") end,
-        { description = "shutdown", group = "awesome" }),
+        { description = "suspend", group = "awesome" }),
     awful.key({ modkey, }, "h", function() awful.tag.incmwfact(0.05) end,
         { description = "increase master width factor", group = "layout" }),
     awful.key({ modkey, }, "l", function() awful.tag.incmwfact(-0.05) end,
